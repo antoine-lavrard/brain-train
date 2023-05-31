1fb68e4ad9525bcad02fc048357afb68bc706515
Mathieu Léonardon <mathieu.leonardon@imt-atlantique.fr>
2023-05-31 15:46:43 +0900

    Remove squeeznet.

--- backbones.py
+++ backbones.py
@@ -248,9 +248,6 @@ class Clip(nn.Module):
     def forward(self, x, mixup = None, lbda = None, perm = None):
         return self.backbone.encode_image(x)
 
-from squeeznet import SqueezeNet
-
-
 def prepareBackbone():
     large = False
     patch_size = 0
@@ -270,8 +267,6 @@ def prepareBackbone():
 
     return {
         "resnet9" : lambda : (ResNet9(args.feature_maps), 5 * args.feature_maps),
-        "squeeznet_1_0": lambda : (SqueezeNet(version="1_0",use_leaky_relu=args.leaky)),
-        "squeeznet_1_1": lambda : (SqueezeNet(version="1_1",use_leaky_relu=args.leaky)),
         "resnet18": lambda: (ResNet(BasicBlock, [(2, 1, 1), (2, 2, 2), (2, 2, 4), (2, 2, 8)], args.feature_maps, large = large), 8 * args.feature_maps),
         "resnet20": lambda: (ResNet(BasicBlock, [(3, 1, 1), (3, 2, 2), (3, 2, 4)], args.feature_maps, large = large), 4 * args.feature_maps),
         "resnet56": lambda: (ResNet(BasicBlock, [(9, 1, 1), (9, 2, 2), (9, 2, 4)], args.feature_maps, large = large), 4 * args.feature_maps),
